774263a2901437a9d3294da987e9a740504e654e
Michael Di Prisco <cadienvan@gmail.com>
2022-12-10 16:37:51 +0100

    chore: updated tests

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,9 +1,5 @@
-// Write a test based on index.ts
-// Run the test
-
 import { sayHello } from '.';
 
-// Make the test pass
 it('should display "Hello World!"', () => {
   expect(sayHello()).toBe('Hello World!');
 });
